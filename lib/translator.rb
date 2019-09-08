9232be0dfad54e3fd7a1c0d4822eea8912884b7f
Keri Deemer <kerid18@att.net>
2019-09-08 20:07:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@
 def load_library
   require 'lib/emoticons.yml'
   
-  load_emoticons = YAML.load(File.read)
+  load_emoticons = YAML.load(File.read("emoticons.yml"))
 end
 
 def get_japanese_emoticon
